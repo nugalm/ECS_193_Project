a445fa14c2e1e743ba3b2eb8bd6c08b1378e7bec
nugalm <jmnugal@ucdavis.edu>
2020-02-01 21:27:15 -0800

    created character class with different element subclasses, each with specific stats according to google doc

---
+++ FireCharacter.js
@@ -0,0 +1,13 @@
+class FireCharacter extends Character {
+    
+    constructor() {        
+        super();
+        this.health = 70;
+        this.power = 70;
+        this.mana = 60;
+        this.speed = 70;
+        this.element = "fire";
+    }
+    
+    
+}
