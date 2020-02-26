5a20f7186937bb105e9cfa9bbade743632093773
Marty Macalalad <mmmacalalad@ucdavis.edu>
2020-02-26 15:24:51 -0800

    Preliminary working for changing the server.js file in handling database collection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var port = 8080;
 //var socketIO = require('socket.io');
 
 var app = express();
@@ -9,8 +10,39 @@ var server = http.Server(app);
 var socketIO = require('socket.io');
 var io = socketIO(server);
 
-app.set('port', 8080);
+app.set('port', port);
 app.use('/static', express.static(__dirname + '/static'));
+
+// Parsing the data and convert to JSON
+// Need to set after port
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connecting the database to a directory
+// Need to set after port and before use
+    // Promise is a confirmation on whether or not
+    // the data was saved into the database
+    // useDefines are for deprecated mongoose
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/project", 
+            {useNewUrlParser: true, useUnifiedTopology: true});
+
+// How to expect the data to be saved
+// Only tentative since this isn't static
+var playerSchema = new mongoose.Schema({
+    name: String,
+    position: {Number, Number},
+    oldPosition: {Number, Number},
+    velocity: {Number, Number},
+    render: Boolean,
+    object: String,
+    rotation: Number,
+});
+
+// User model associated with the schema
+var Player = mongoose.model("Player", playerSchema);
 
 // Routing
 app.get('/', function(request, response) {
@@ -19,6 +51,21 @@ app.get('/', function(request, response) {
 
 server.listen(8080, function() {
     console.log('Starting server on port 8080');
+});
+/*
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
+*/
+app.post("/", (request,response) => {
+    var data = new Player(request.body);
+    data.save()
+        .then(item => {
+        response.send("Updated player")
+    })
+    .catch(err => {
+        response.status(400).send("Failed to send to database");
+    });
 });
 
 var serverPlayers = {};
