8f509de1fcf849f7352dbab91380b1790f6a9af2
nugalm <jmnugal@ucdavis.edu>
2020-05-07 20:18:26 -0700

    added shotgun cone fire five bullets

--- static/projectileHandler.js
+++ static/projectileHandler.js
@@ -4,6 +4,9 @@ class ProjectileHandler
     constructor(context)
     {
         this.context = context;
+        
+        this.numSaltBullets = 5;
+        this.saltBulletSize = 15;
     }
     
     initProjectiles()
@@ -14,23 +17,42 @@ class ProjectileHandler
     createProjectile()
     {
         this.initProjectile();
-       // var projectile = this.context.projectiles.create(this.context.player.myContainer.x, this.context.player.myContainer.y, 'projectile');
-     //   projectile.setCollideWorldBounds(false);
-     //   projectile.body.setAllowGravity(false); 
-     //   projectile.rotation = this.context.player.sprite.rotation - (Math.PI / 2);
-     //   projectile.element = this.context.player.element;
-       
     }
     
     initProjectile()
     {
-        
-        var projectile = this.context.physics.add.sprite(this.context.player.myContainer.x, this.context.player.myContainer.y, 'projectile');
-        projectile.rotation = this.context.player.sprite.rotation - (Math.PI / 2);
-        projectile.element = this.context.player.element;
-        this.context.projectiles.add(projectile);
+        if (this.context.player.gun == "salt_shaker") 
+        {
+            this.initSaltProjectiles();
+                      
+        }
+        else {
+            var projectile = this.context.physics.add.sprite(this.context.player.myContainer.x, this.context.player.myContainer.y, 'projectile');
+            projectile.rotation = this.context.player.sprite.rotation - (Math.PI / 2);
+            projectile.element = this.context.player.element;
+            this.context.projectiles.add(projectile);
+        }
         
     }
+    
+    initSaltProjectiles()
+    {
+        var i;
+        for (i = 0; i < 5; i++) 
+        {
+            var projectile = this.context.physics.add.sprite(this.context.player.myContainer.x, this.context.player.myContainer.y, 'projectile');
+            projectile.rotation = this.context.player.sprite.rotation - ( (Math.PI / 3) + (i*(Math.PI / 12)) );
+            projectile.element = this.context.player.element;
+            projectile.salt = true;
+            projectile.setDisplaySize(this.saltBulletSize, this.saltBulletSize);
+            this.context.projectiles.add(projectile);
+            
+        }
+        
+    }
+    
+    
+    
     moveProjectiles()
     {
         this.context.projectiles.children.iterate(function(child) {
