f22de21fbff9211ef6d38afbb9f7eb1c8af9170f
nugalm <jmnugal@ucdavis.edu>
2020-05-13 14:29:45 -0700

    added lifespans to bullets

--- static/projectileHandler.js
+++ static/projectileHandler.js
@@ -9,7 +9,7 @@ class ProjectileHandler
         this.saltBulletSize = 15;
         
         // time it takes for projectile to disappear (in milliseconds)
-        this.saltTime = 1000;
+        this.saltTime = 500;
         this.bottleTime = 2000;
         this.frostingTime = 3000;
         
@@ -33,6 +33,14 @@ class ProjectileHandler
             this.initSaltProjectiles();
                       
         }
+        else if (this.context.player.gun == "bottle")
+        {
+            this.initBottleProjectiles();
+        }
+        else if (this.context.player.gun == "frosting_bag")
+        {
+            this.initFrostingProjectiles();
+        }
         else {
             var projectile = this.context.physics.add.sprite(this.context.player.myContainer.x, this.context.player.myContainer.y, 'projectile');
             projectile.rotation = this.context.player.sprite.rotation - (Math.PI / 2);
@@ -51,15 +59,48 @@ class ProjectileHandler
             var projectile = new Projectile({scene: this.context, x: this.context.player.myContainer.x, y: this.context.player.myContainer.y, key: "projectile"}, "salt")
             projectile.rotation = this.context.player.sprite.rotation - ( (Math.PI / 3) + (i*(Math.PI / 12)) );
             projectile.element = this.context.player.element;
-            //projectile.salt = true;
-            //projectile.lifespan = 250;
             projectile.setDisplaySize(this.saltBulletSize, this.saltBulletSize);
             this.context.projectiles.add(projectile);
-            this.context.physics.world.enable(projectile);
-            
+            projectile.salt = true;
+            this.setDeletionTimer(projectile);
+            //this.context.physics.world.enable(projectile);
         }
         
     }
+    
+    initBottleProjectiles()
+    {
+        
+        
+            //var projectile = this.context.physics.add.sprite(this.context.player.myContainer.x, this.context.player.myContainer.y, 'projectile');
+            var projectile = new Projectile({scene: this.context, x: this.context.player.myContainer.x, y: this.context.player.myContainer.y, key: "projectile"}, "bottle")
+            projectile.rotation = this.context.player.sprite.rotation - (Math.PI / 2);
+            projectile.element = this.context.player.element;
+            this.context.projectiles.add(projectile);
+            projectile.bottle = true;
+            this.setDeletionTimer(projectile);
+            //this.context.physics.world.enable(projectile);
+        
+        
+    }
+    
+    initFrostingProjectiles()
+    {
+        
+        
+            //var projectile = this.context.physics.add.sprite(this.context.player.myContainer.x, this.context.player.myContainer.y, 'projectile');
+            var projectile = new Projectile({scene: this.context, x: this.context.player.myContainer.x, y: this.context.player.myContainer.y, key: "projectile"}, "frosting")
+            projectile.rotation = this.context.player.sprite.rotation - (Math.PI / 2);
+            projectile.element = this.context.player.element;
+            this.context.projectiles.add(projectile);
+            projectile.frosting = true;
+            this.setDeletionTimer(projectile);
+            //this.context.physics.world.enable(projectile);
+        
+        
+    }
+    
+    
     
     setDeletionTimer(projectile)
     {
@@ -68,24 +109,42 @@ class ProjectileHandler
             this.context.time.addEvent 
             ({
                 delay: this.saltTime,
+                args: [projectile],
                 callback: this.callbackFunction,
-                callbackScope: this.context,
+                callbackScope: this,
                 loop: false
             });
         }
         else if (projectile.bottle == true)
         {
-            
+            this.context.time.addEvent 
+            ({
+                delay: this.bottleTime,
+                args: [projectile],
+                callback: this.callbackFunction,
+                callbackScope: this,
+                loop: false
+            });
         }
         else if(projectile.frosting == true)
         {
-            
+            this.context.time.addEvent 
+            ({
+                delay: this.frostingTime,
+                args: [projectile],
+                callback: this.callbackFunction,
+                callbackScope: this,
+                loop: false
+            });
         }
     }
     
-    callbackFunction()
+    
+    
+    callbackFunction(projectile)
     {
-        
+        //alert("destroying projectile");
+        projectile.destroy();
     }
     
     moveProjectiles()
